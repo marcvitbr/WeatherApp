407f759693007b661eb09fd27969366a825ee6be
Marcelo Vitoria <marcelo.vitoria@mindera.com>
2021-01-19 21:00:42 +0000

    Test forecast fetching in temp ViewController

--- Sources/App/Shared/ViewController.swift
+++ Sources/App/Shared/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var fetchForecast: FetchForecast = {
+
+        FetchForecast(fetcher: DefaultForecastFetcher(httpClient: DefaultHTTPClient(),
+                                                      forecastConverter: DefaultForecastConverter()))
+    }()
+
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let porto = City(latitude: 41.1552, longitude: -8.6326)
+
+        self.fetchForecast.execute(with: porto) { result in
+
+            switch result {
+
+            case .success(let forecast):
+
+                print(forecast)
+                break
+            case .failure(let error):
+
+                print(error.localizedDescription)
+                break
+            }
+        }
     }
-
-
 }
 
