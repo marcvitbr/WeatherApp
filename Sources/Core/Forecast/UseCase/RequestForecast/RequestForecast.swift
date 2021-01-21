c5c943153dc82fb129f772620a821e16b6ad7bd4
Marcelo Vitoria <marcelo.vitoria@mindera.com>
2021-01-21 23:57:05 +0000

    Remove ParameterizedUseCase protocol

--- Sources/Core/Forecast/UseCase/RequestForecast/RequestForecast.swift
+++ Sources/Core/Forecast/UseCase/RequestForecast/RequestForecast.swift
@@ -5,7 +5,7 @@
 //  Created by Marcelo Vitoria on 17/01/2021.
 //
 
-protocol RequestForecast: ParameterizedUseCase {
+protocol RequestForecast {
 
     func execute(with input: City, receiver: @escaping (RequestForecastResult) -> ())
 }
