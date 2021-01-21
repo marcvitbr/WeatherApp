584cf189aa4d4e254614c8867eeaeed1e3af2d45
Marcelo Vitoria <marcelo.vitoria@mindera.com>
2021-01-21 16:52:07 +0000

    Test FetchForecast call in ForecastViewController

--- Sources/App/Presentation/Forecast/Controller/ForecastViewController.swift
+++ Sources/App/Presentation/Forecast/Controller/ForecastViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ForecastViewController: UIViewController {
 
@@ -15,21 +16,71 @@ class ForecastViewController: UIViewController {
     @IBOutlet weak var day3View: DayView!
     @IBOutlet weak var day4View: DayView!
 
+    private lazy var fetchForecast: FetchForecast = {
+
+        FetchForecast(fetcher: DefaultForecastFetcher(httpClient: DefaultHTTPClient(),
+                                                      forecastConverter: DefaultForecastConverter()))
+    }()
+
+    private lazy var dateFormatter: DateFormatter = {
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
+        return dateFormatter
+    }()
+
+    private lazy var dayFormatter: DateFormatter = {
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE"
+        return dateFormatter
+    }()
+
+    private lazy var numberFormatter: NumberFormatter = {
+
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .none
+        return numberFormatter
+    }()
+
     override func viewDidLoad() {
+
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let porto = City(latitude: 41.1552, longitude: -8.6326)
+
+        self.fetchForecast.execute(with: porto) { [weak self] result in
+
+            switch result {
+
+            case .success(let forecast):
+
+                DispatchQueue.main.async { self?.presentForecast(forecast) }
+            case .failure(let error):
+
+                print(error.localizedDescription)
+            }
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func presentForecast(_ forecast: Forecast) {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        self.currentDayView.cityNameLabel.text = "Porto"
+        self.currentDayView.dateLabel.text = self.dateFormatter.string(from: forecast.current.date)
+        self.currentDayView.temperatureLabel.text = (self.numberFormatter.string(from: NSNumber(value: forecast.current.temperatureValue)) ?? "") + "º"
+        self.currentDayView.weatherLabel.text = forecast.current.weather?.title ?? "--"
+
+        let daysViews = [self.day1View, self.day2View, self.day3View, self.day4View]
+
+        for i in 0..<daysViews.count {
+
+            let day = forecast.days[i]
+
+            guard let dayView = daysViews[i] else { continue }
+
+            dayView.weekDayLabel.text = self.dayFormatter.string(from: day.date).uppercased()
+            dayView.minTemperatureLabel.text = (self.numberFormatter.string(from: NSNumber(value: day.temperature.min)) ?? "") + "º"
+            dayView.maxTemperatureLabel.text = (self.numberFormatter.string(from: NSNumber(value: day.temperature.max)) ?? "") + "º"
+        }
     }
-    */
-
 }
